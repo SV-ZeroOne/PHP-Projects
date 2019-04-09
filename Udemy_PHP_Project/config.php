95c64c3caa70c3844a24e3c025fec7621c3fbadc
Steve Velcev <steve@elitehost.co.za>
2019-04-09 17:53:09 +0200

    chore: add Shareboard initial project model and controller class files

---
+++ Udemy_PHP_Project/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+//Define URLS
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http://localhost/PHP-Projects/Udemy_PHP_Project/");
+
+?>
---
+++ Udemy_PHP_Project/views/main.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Shareboard</title>
+</head>
+<body>
+    
+</body>
+</html>
